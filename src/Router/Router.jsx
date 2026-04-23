fdb63d75c5b771cb0c649a118170a9cd199ef527
Abhishek Gupta <amansg710@gmail.com>
2026-04-23 15:51:06 +0530

    Update Router.jsx

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,23 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
-
 import MainLayout from "../layouts/MainLayout";
 import Home from "../pages/Home/Home";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <MainLayout />,
-        children: [
-            { path: "", element: <Home /> },
-        ],
-    },
-    {
-        path: "/portfolio", // GitHub Pages repo name compatibility
-        element: <MainLayout />,
-        children: [
-            { path: "", element: <Home /> },
-        ],
-    },
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      { index: true, element: <Home /> }, // cleaner than path: ""
+    ],
+  },
 ]);
 
 export default router;
